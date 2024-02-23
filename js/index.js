537fe1120e02ed1f02ba959b273b0d6e3bab97b8
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-02-23 15:27:58 +1300

    commenting

--- js/index.js
+++ js/index.js
@@ -9,12 +9,26 @@ async function testSummon() {
 }
 // ------------------------------------------------------
 
-function capitalizeFirstLetter(string){
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
+// Get Data ---------------------------------------------
 
-// 
+/*
+    Returns the json of a specific file.
+*/
 async function getJsonData(filename) {
     const response = await fetch(filename);
     return await response.json();
 }
+
+// ------------------------------------------------------
+
+// Utilities --------------------------------------------
+
+/*
+    Takes a string and capitalizes the first letter.
+    eg. "hello" -> "Hello"
+*/
+function capitalizeFirstLetter(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+// ------------------------------------------------------
